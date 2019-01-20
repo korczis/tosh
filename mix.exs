4ec0e93a68893a0123674e2b2cb90338f6577224
Tomas Korcak <korczis@gmail.com>
2019-01-20 03:37:10 +0100

    mix.exs - add description, license, maintainers

--- mix.exs
+++ mix.exs
@@ -4,12 +4,15 @@ defmodule Tosh.MixProject do
   def project do
     [
       app: :tosh,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "0.0.1",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
+      description: description(),
+      docs: docs(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -26,7 +29,49 @@ defmodule Tosh.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
+
   defp elixirc_paths(_), do: ["lib"]
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+    ]
+  end
+
+  def description do
+    """
+    Another Web Application Template
+    """
+  end
+
+  defp docs do
+    [
+      source_url: "https://github.com/vic/spec",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package do
+    [
+      files: [
+        "lib",
+        "mix.exs",
+        "README*"
+      ],
+      maintainers: [
+        "Tomas Korcak <korczis@gmail.com>"
+      ],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/korczis/tosh"}]
+  end
 
   # Specifies your project dependencies.
   #
@@ -43,20 +88,6 @@ defmodule Tosh.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
-    ]
-  end
-
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to create, migrate and run the seeds file at once:
-  #
-  #     $ mix ecto.setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
-  defp aliases do
-    [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
