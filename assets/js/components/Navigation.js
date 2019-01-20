dc197f223b460e5769ceaa47d8058202aeab66b8
Tomas Korcak <korczis@gmail.com>
2019-01-20 23:31:07 +0100

    Use reactstrap for navigation

--- assets/js/components/Navigation.js
+++ assets/js/components/Navigation.js
@@ -1,63 +1,79 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavLink,
+    NavItem,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap'
 
 export class Navigation extends Component {
+    constructor(props) {
+        super(props)
+
+        this.toggle = this.toggle.bind(this)
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="/">Tosh</a>
+            <Navbar color="light" light expand="lg">
+                <NavbarBrand href="/">Tosh</NavbarBrand>
 
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                <NavbarToggler onClick={this.toggle} />
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" onClick={() => push('/intro')}>
-                                Intro <span className="sr-only">(current)</span>
-                            </a>
-                        </li>
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink to="/intro" tag={Link}>Intro</NavLink>
+                        </NavItem>
 
-                        <li className="nav-item">
-                            <a className="nav-link" href="/home">Home</a>
-                        </li>
+                        <NavItem>
+                            <NavLink to="/home" tag={Link}>Home</NavLink>
+                        </NavItem>
 
-                        <li className="nav-item">
-                            <a className="nav-link" href="/about">About</a>
-                        </li>
+                        <NavItem>
+                            <NavLink to="/about" tag={Link}>About</NavLink>
+                        </NavItem>
 
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <UncontrolledDropdown>
+                            <DropdownToggle>
                                 Dropdown
-                            </a>
+                            </DropdownToggle>
 
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a className="dropdown-item" href="/">Action</a>
-                                <a className="dropdown-item" href="/">Another action</a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="/">Something else here</a>
-                            </div>
-                        </li>
+                            <DropdownMenu >
+                                <DropdownItem href="/">Action</DropdownItem>
+                                <DropdownItem href="/">Another action</DropdownItem>
+                                <DropdownItem href="/">Something else here</DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
 
-                        <li className="nav-item">
-                            <a className="nav-link disabled" href="." tabIndex="-1" aria-disabled="true">Disabled</a>
-                        </li>
+                        <NavItem>
+                            <NavLink to="/about" tag={Link} disabled>About</NavLink>
+                        </NavItem>
 
                         <form className="form-inline my-2 my-lg-0">
                             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                         </form>
-                    </ul>
-                </div>
-            </nav>
+                    </Nav>
+                </Collapse>
+            </Navbar>
         );
     }
 }
