0cc1900c4ba1805b0668b457ac161d8101fe01dd
Tomas Korcak <korczis@gmail.com>
2019-01-20 22:54:41 +0100

    Use onClick()

--- assets/js/components/Navigation.js
+++ assets/js/components/Navigation.js
@@ -20,7 +20,7 @@ export class Navigation extends Component {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="/intro">
+                            <a className="nav-link" onClick={() => push('/intro')}>
                                 Intro <span className="sr-only">(current)</span>
                             </a>
                         </li>
@@ -34,11 +34,11 @@ export class Navigation extends Component {
                         </li>
 
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button"
+                            <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button"
                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Dropdown
                             </a>
-                            
+
                             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <a className="dropdown-item" href="/">Action</a>
                                 <a className="dropdown-item" href="/">Another action</a>
